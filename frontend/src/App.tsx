afc90488fec735d0b4d16f528e35571abccf2b0e
Alexandre Moraes da Silva <alexandre@moraesdasilva.com.br>
2022-01-10 20:27:05 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
+import About from 'pages/About';
 import Navbar from 'components/Navbar';
 
 function App(): JSX.Element {
@@ -12,6 +13,7 @@ function App(): JSX.Element {
         <Route path='/form'>
           <Route path=':movieId' element={<Form />} />
         </Route>
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );
