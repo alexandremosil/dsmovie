fc9f4e812026fdd0b869527885c10785731dbdf4
Alexandre Moraes da Silva <alexandre@moraesdasilva.com.br>
2022-01-16 19:04:41 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,15 +2,37 @@ import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
 import axios from 'axios';
 import { BASE_URL } from 'utils/requests';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
 
 function Listing() {
+  // useState hook
+  const [pageNumber, setPageNumber] = useState(0);
+
   // FORMA ERRADA!
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-    console.log(response.data);
-  });
+  // Chama backend duas vezes
+  // Passa pela função mais de uma vez se não estiver ligado ao ciclo de vida
+  // axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+  //   console.log(response.data);
+  // });
+  // ou
+  //axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+  //  const data = response.data as MoviePage;
+  //  setPageNumber(data.number);
+  //});
+
+  // Utilizar userEffect
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data); // Exibe no console apenas uma vez
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className='container'>
         <div className='row'>
